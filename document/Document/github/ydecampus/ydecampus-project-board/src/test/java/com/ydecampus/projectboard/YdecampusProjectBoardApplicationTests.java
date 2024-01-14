1ac76e02bada9a234190001742477cc498e82a23
jieun <yudaeun222@gmail.com>
2024-01-14 23:47:11 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ document/Document/github/ydecampus/ydecampus-project-board/src/test/java/com/ydecampus/projectboard/YdecampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydecampus.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YdeCampusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
